25877fe6f1ea18f8506bbb571f2c5e2a598c11cd
Sam Codrington <sam.codrington@queensu.ca>
2017-03-30 03:01:21 -0400

    ctl_unit transplanted into cpu

--- cpu_codyale.vhd
+++ cpu_codyale.vhd
@@ -6,7 +6,7 @@ LIBRARY work;
 ENTITY cpu_codyale IS
 	PORT ( 		
 		--CONTROL PORTS
-			clk,	reset, stop : IN STD_LOGIC;
+			clk,	reset, stop, Strobe : IN STD_LOGIC;
 			InPort	: IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 		--END CTL PORTS
 		
@@ -201,20 +201,20 @@ BEGIN
 		Cout => w_Cout, 	BAout => w_BAout,
 		
 		--ALU Signals
-		ADD, SUB, ANDop, ORop, 
-		SHR, SHL, ROTR, ROTL,
-		MUL, DIV, NEG, NOTop, 
-		IncPC, claADD,
+		ADD =>w_ADD,	SUB =>w_SUB,	ANDop=>w_ANDop,	ORop=>w_ORop,
+		SHR =>w_SHR,	SHL =>w_SHL,	ROTR=>w_ROTR,		ROTL=>w_ROTL,
+		MUL =>w_MUL,	DIV =>w_DIV, 	NEG =>w_NEG, 		NOTop=>w_NOTop,
+		IncPC=>w_IncPC,	claADD=>w_claADD,
 		
 		--Memory Signals
-		ReadSig, WriteSig
+		ReadSig => w_ReadSig, WriteSig => w_WriteSig
 	);
 	
 	con_ff_inst : con_ff
 	PORT MAP(
 		busin	=>w_BusMuxOut,
 	   IRin	=>w_IRout(1 downto 0),
-	   CONin =>CONin,
+	   CONin =>w_CONin,
 	   Q		=>w_con_ff_out
 	);
 	sel_encode_inst : sel_encode
@@ -230,9 +230,9 @@ BEGIN
 		RIN_output => RIn_sel,
 		Rout_output => Rout_sel		
 	);
-	MAR : reg32 			PORT MAP (input => w_BusMuxOut, 	clr=>w_clr,	clk=>clk,	reg_in=>MARin, output=> MARout); -- BUS to RAM
+	MAR : reg32 			PORT MAP (input =>w_BusMuxOut, 	clr=>w_clr,	clk=>clk,	reg_in=>w_MARin, output=> MARout); -- BUS to RAM
 	InPort_inst : reg32 	PORT MAP(input =>InPort,		clr=>w_clr,	clk=>clk,	reg_in=>Strobe, output=> BusMuxInPort);
-	OutPort_inst: reg32	PORT MAP(input =>w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>OutPort_en, output=> OutPort);
+	OutPort_inst: reg32	PORT MAP(input =>w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>w_OutPort_en, output=> OutPort);
 	
 	
 	MDR_inst : MDR
@@ -241,8 +241,8 @@ BEGIN
 		MDataIn	=> MDataIn,
 		clr 		=> w_clr,
 		clk		=> clk,
-		mdr_in	=> MDRin,
-		MDRread	=> MemRead,
+		mdr_in	=> w_MDRin,
+		MDRread	=> w_ReadSig,
 		output	=> BusMuxInMDR					
 	);
 	ram_inst : ram
@@ -250,8 +250,8 @@ BEGIN
 		address=> MARout(8 downto 0),
 		clock	=> clk,
 		data	=> BusMuxInMDR,
-		rden	=> MemRead,
-		wren	=> WriteSig,
+		rden	=> w_ReadSig,
+		wren	=> w_WriteSig,
 		q		 => MDataIn
 	);
 	--Registers
@@ -272,13 +272,13 @@ BEGIN
 	R14 : reg32	PORT MAP (input => w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>w_R14_en,				output=> BusMuxInR14);
 	R15 : reg32	PORT MAP (input => w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>Rin_sel(15),		output=> BusMuxInR15);
 
-	HI : reg32  PORT MAP (input => w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>HIin,	output=> BusMuxInHI);	-- to/from BUS
-	LO : reg32	PORT MAP (input => w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>LOin,	output=> BusMuxInLO); -- to/from BUS
-	ZHI : reg32	PORT MAP (input => w_alu2z(63 downto 32),		clr=>w_clr,	clk=>clk,	reg_in=>	Zin,	output=> BusMuxInZHI); -- FROM ALU to BUS
- 	ZLO : reg32	PORT MAP (input => w_alu2z(31 downto 0),		clr=>w_clr,	clk=>clk,	reg_in=>	Zin,	output=> BusMuxInZLO); -- FROM ALU to BUS	
-	Y  : reg32  PORT MAP (input => w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=> Yin, 	output=> w_y2ALU); -- FROM BUS TO ALU
-	PC : reg32	PORT MAP (input => w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>	PCin,	output=> BusMuxInPC); --to/from BUS
-	IR : reg32  PORT MAP (input => w_BusMuxOut, 	clr=>w_clr,	clk=>clk,	reg_in=>	IRin,	output=> w_IRout); --from BUS to OUT??
+	HI : reg32  PORT MAP (input => w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>w_HIin,	output=> BusMuxInHI);	-- to/from BUS
+	LO : reg32	PORT MAP (input => w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>w_LOin,	output=> BusMuxInLO); -- to/from BUS
+	ZHI : reg32	PORT MAP (input => w_alu2z(63 downto 32),		clr=>w_clr,	clk=>clk,	reg_in=>w_Zin,	output=> BusMuxInZHI); -- FROM ALU to BUS
+ 	ZLO : reg32	PORT MAP (input => w_alu2z(31 downto 0),		clr=>w_clr,	clk=>clk,	reg_in=>w_Zin,	output=> BusMuxInZLO); -- FROM ALU to BUS	
+	Y  : reg32  PORT MAP (input => w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>w_Yin, 	output=> w_y2ALU); -- FROM BUS TO ALU
+	PC : reg32	PORT MAP (input => w_BusMuxOut,	clr=>w_clr,	clk=>clk,	reg_in=>w_PCin,	output=> BusMuxInPC); --to/from BUS
+	IR : reg32  PORT MAP (input => w_BusMuxOut, 	clr=>w_clr,	clk=>clk,	reg_in=>w_IRin,	output=> w_IRout); --from BUS to OUT??
 
 	cpu_bus_inst : cpu_bus
 	PORT MAP(
@@ -286,8 +286,8 @@ BEGIN
 		R04out=>Rout_sel(4), 	R05out=>Rout_sel(5),		R06out=>Rout_sel(6),		R07out=>Rout_sel(7),
 		R08out=>Rout_sel(8),		R09out=>Rout_sel(9),		R10out=>Rout_sel(10),	R11out=>Rout_sel(11),
 		R12out=>Rout_sel(12),	R13out=>Rout_sel(13),	R14out=>Rout_sel(14),	R15out=>Rout_sel(15),
-		hiout=>hiout,		loout=>loout,		zhiout=>zhiout,	zloout=>zloout,
-		pcout=>pcout, 		mdrout=>MDROut,	portout=>PortOut, cOut=>Cout,
+		hiout=>w_Hiout,		loout=>w_Loout,		zhiout=>w_ZHiOut,	zloout=>w_ZLoOut,
+		pcout=>w_PCout, 		mdrout=>w_MDROut,	portout=>w_InportOut, cOut=>w_Cout,
 		
 		r00in=>BusMuxInR00,	r01in=>BusMuxInR01,	r02in=>BusMuxInR02,	r03in=>BusMuxInR03,
 		r04in=>BusMuxInR04,	r05in=>BusMuxInR05,	r06in=>BusMuxInR06,	r07in=>BusMuxInR07, 
@@ -305,7 +305,7 @@ BEGIN
 		Zout => w_alu2z
 	);
 	
-	process(clk,clr,w_BusMuxOut,	w_con_ff_out,	Rin_sel,		MARout,		RA_en,
+	process(clk,w_clr,w_BusMuxOut,	w_con_ff_out,	Rin_sel,		MARout,		w_RA_en,
 	BusMuxInR00,	BusMuxInR01,	BusMuxInR02,	BusMuxInR03,
 	BusMuxInR04,	BusMuxInR05,	BusMuxInR06,	BusMuxInR07,
 	BusMuxInR08,	BusMuxInR09,	BusMuxInR10,	BusMuxInR11,
@@ -314,7 +314,7 @@ BEGIN
 	BusMuxInPC,		BusMuxInMDR,	BusMuxInPort,	BusMuxInC, 
 	w_IRout,	w_y2ALU )
 	begin
-		w_R14_en <= (Rin_sel(14) or RA_en);
+		w_R14_en <= (Rin_sel(14) or w_RA_en);
 		d_R00Out <= BusMuxInR00;
 		d_R01Out <= BusMuxInR01;
 		d_R02Out <= BusMuxInR02;
