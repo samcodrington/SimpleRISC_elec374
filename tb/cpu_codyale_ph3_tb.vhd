91a6a8e7f4888d526245d6d034afa57eb5f11fe9
Sam Codrington <sam.codrington@queensu.ca>
2017-04-02 16:28:14 -0400

    turned stop off, rearranged signals for better visibility

--- tb/cpu_codyale_ph3_tb.vhd
+++ tb/cpu_codyale_ph3_tb.vhd
@@ -7,10 +7,13 @@ END ENTITY;
 ARCHITECTURE behavioural OF cpu_codyale_ph3_tb IS 
 
 SIGNAL clk_tb, reset_tb, stop_tb, Strobe_tb, d_Run_tb : std_logic;
-SIGNAL Inport_tb, OutPort_tb, 	d_R00Out_tb,	d_R01Out_tb,	d_R02Out_tb,	d_R03Out_tb,	d_R04Out_tb,	d_R05Out_tb,	d_R06Out_tb,	d_R07Out_tb,
+SIGNAL Inport_tb, OutPort_tb, 	
+		d_PCOut_tb,		d_IROut_tb,		d_MDROut_tb,	d_MARout_tb,
+		d_C_sign_extended_tb,			d_BusMuxOut_tb,d_YOut_tb,		
+		d_ZLoOut_tb, 	d_ZHiOut_tb,	
+		d_R00Out_tb,	d_R01Out_tb,	d_R02Out_tb,	d_R03Out_tb,	d_R04Out_tb,	d_R05Out_tb,	d_R06Out_tb,	d_R07Out_tb,
 		d_R08Out_tb,	d_R09Out_tb,	d_R10Out_tb,	d_R11Out_tb,	d_R12Out_tb,	d_R13Out_tb,	d_R14Out_tb,	d_R15Out_tb,
-		d_HIOut_tb,		d_LOOut_tb,		d_PCOut_tb,		d_MDROut_tb,	d_BusMuxOut_tb, d_IROut_tb, 	d_YOut_tb,		d_C_sign_extended_tb,
-		d_ZLoOut_tb, 	d_ZHiOut_tb,	d_MARout_tb		: std_logic_vector(31 downto 0); 
+		d_HIOut_tb,		d_LOOut_tb		: std_logic_vector(31 downto 0); 
 		
 COMPONENT cpu_codyale
 	PORT(
@@ -60,7 +63,7 @@ BEGIN
 	BEGIN
 		reset_tb <= '1'; stop_tb <='1';
 		wait until rising_edge(clk_tb);
-		reset_tb <= '0';
+		reset_tb <= '0'; stop_tb <='0';
 		wait;
 	END PROCESS;
 END;
